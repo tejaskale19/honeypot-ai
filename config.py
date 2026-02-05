32a8f43e7d7ad3e6db8a1f3d75e2fdd0939137c3
Tejas Kale <ktejas.2006@gmail.com>
2026-02-05 23:18:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-# Load variables from .env file
 load_dotenv()
 
 class Settings:
     APP_NAME: str = os.getenv("APP_NAME", "Honeypot-AI")
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
-    HONEYPOT_API_KEY: str = os.getenv("HONEYPOT_API_KEY")
-
-    if not HONEYPOT_API_KEY:
-        raise ValueError("HONEYPOT_API_KEY is missing in .env file")
+    HONEYPOT_API_KEY: str = os.getenv("HONEYPOT_API_KEY", "guvi-demo-key-123")
 
 settings = Settings()
+
